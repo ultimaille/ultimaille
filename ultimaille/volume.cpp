1ad58ed9428370819220e793ee9184aedbd2215f
Dmitry V. Sokolov <ha@haqr.eu>
2020-10-27 22:54:07 +0100

    volume mesh stub

---
+++ ultimaille/volume.cpp
@@ -0,0 +1,343 @@
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include "volume.h"
+#include "attributes.h"
+
+namespace UM {
+    void Volume::resize_attrs() {
+    /*
+        for (auto &wp : attr_facets)  if (auto spt = wp.lock())
+            spt->resize(nfacets());
+        for (auto &wp : attr_corners) if (auto spt = wp.lock())
+            spt->resize(ncorners());
+    */
+    }
+
+    void Volume::compress_attrs(const std::vector<bool> &cells_to_kill) {
+    /*
+        assert(facets_to_kill.size()==(size_t)nfacets());
+        std::vector<int>  facets_old2new(nfacets(),  -1);
+        std::vector<int> corners_old2new(ncorners(), -1);
+
+        int new_nb_facets  = 0;
+        int new_nb_corners = 0;
+
+        for (int f=0; f<nfacets(); f++) {
+            if (facets_to_kill[f]) continue;
+            for (int lv=0; lv<facet_size(f); lv++)
+                corners_old2new[facet_corner(f, lv)] = new_nb_corners++;
+            facets_old2new[f] = new_nb_facets++;
+        }
+        std::cerr << "compressing facet attributes\n";
+        for (auto &wp : attr_facets)  if (auto spt = wp.lock())
+            spt->compress(facets_old2new);
+        std::cerr << "compressing corner attributes\n";
+        for (auto &wp : attr_corners) if (auto spt = wp.lock())
+            spt->compress(corners_old2new);
+       */
+    }
+
+    int Volume::nverts() const {
+        return points.size();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    int Tetrahedra::ncells() const {
+        return cells.size()/4;
+    }
+
+    int Tetrahedra::create_tets(const int n) {
+        cells.resize(cells.size()+n*4);
+        resize_attrs();
+        return ncells()-n;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    int Hexahedra::ncells() const {
+        return cells.size()/8;
+    }
+
+    int Hexahedra::create_hexa(const int n) {
+        cells.resize(cells.size()+n*8);
+        resize_attrs();
+        return ncells()-n;
+    }
+
+
+/*
+
+
+	int Surface::ncorners() const {
+		return facets.size();
+	}
+
+	void Surface::delete_vertices(const std::vector<bool> &to_kill) {
+		std::vector<bool> facets_to_kill(nfacets(), false);
+		SurfaceConnectivity fec(*this);
+
+		for (int v=0; v<nverts(); v++) {
+		    if (!to_kill[v]) continue;
+		    int cir = fec.v2c[v];
+		    if (cir<0) continue; // isolated vertex
+		    do {
+		        facets_to_kill[fec.c2f[cir]] = true;
+		        cir = fec.c2c[cir];
+		    } while (cir != fec.v2c[v]);
+		}
+		delete_facets(facets_to_kill);
+
+		std::vector<int> old2new;
+		points.delete_points(to_kill, old2new);
+		for (int &v : facets)
+		    v = old2new[v];
+	}
+
+	void Surface::delete_facets(const std::vector<bool> &to_kill) {
+		assert(to_kill.size()==(size_t)nfacets());
+		compress_attrs(to_kill);
+
+		int new_nb_facets  = 0;
+		int new_nb_corners = 0;
+		for (int f=0; f<nfacets(); f++) {
+		    if (to_kill[f]) continue;
+		    for (int lv=0; lv<facet_size(f); lv++)
+		        facets[new_nb_corners++] = vert(f, lv);
+		    ++new_nb_facets;
+		}
+		facets.resize(new_nb_corners);
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	int Triangles::create_facets(const int n) {
+		facets.resize(facets.size()+n*3);
+		resize_attrs();
+		return nfacets()-n;
+	}
+
+	int Triangles::nfacets() const {
+		assert(0==facets.size()%3);
+		return facets.size()/3;
+	}
+
+	int Triangles::facet_size(const int) const {
+		return 3;
+	}
+
+	int Triangles::facet_corner(const int fi, const int ci) const {
+		assert(ci>=0 && ci<3 && fi>=0 && fi<nfacets());
+		return fi*3 + ci;
+	}
+
+	int Triangles::vert(const int fi, const int lv) const {
+		assert(fi>=0 && fi<nfacets() && lv>=0 && lv<3);
+		return facets[fi*3 + lv];
+	}
+
+	int &Triangles::vert(const int fi, const int lv) {
+		assert(fi>=0 && fi<nfacets() && lv>=0 && lv<3);
+		return facets[fi*3 + lv];
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	int Quads::create_facets(const int n) {
+		facets.resize(facets.size()+n*4);
+		resize_attrs();
+		return nfacets()-n;
+	}
+
+	int Quads::nfacets() const {
+		assert(0==facets.size()%4);
+		return facets.size()/4;
+	}
+
+	int Quads::facet_size(const int) const {
+		return 4;
+	}
+
+	int Quads::facet_corner(const int fi, const int ci) const {
+		assert(ci>=0 && ci<4 && fi>=0 && fi<nfacets());
+		return fi*4 + ci;
+	}
+
+	int Quads::vert(const int fi, const int lv) const {
+		assert(fi>=0 && fi<nfacets() && lv>=0 && lv<4);
+		return facets[fi*4 + lv];
+	}
+
+	int &Quads::vert(const int fi, const int lv) {
+		assert(fi>=0 && fi<nfacets() && lv>=0 && lv<4);
+		return facets[fi*4 + lv];
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Polygons::Polygons() : Surface(), offset(1, 0) {}
+
+	int Polygons::create_facets(const int n, const int size) {
+		for (int i=0; i<n*size; i++)
+		    facets.push_back(0);
+		for (int i=0; i<n; i++)
+		    offset.push_back(offset.back()+size);
+		resize_attrs();
+		return nfacets()-n;
+	}
+
+	void Polygons::delete_facets(const std::vector<bool> &to_kill) {
+		Surface::delete_facets(to_kill);
+		int new_nb_facets = 0;
+		for (int f=0; f<nfacets(); f++) {
+		    if (to_kill[f]) continue;
+		    offset[new_nb_facets+1] = offset[new_nb_facets] + facet_size(f);
+		    ++new_nb_facets;
+		}
+		offset.resize(new_nb_facets+1);
+	}
+
+	void Polygons::extract_triangles(Triangles &tri) {
+		tri = Triangles();
+		tri.points = points;
+		int ntri = 0;
+		for (int f=0; f<nfacets(); f++)
+		    ntri += (3==facet_size(f));
+		tri.create_facets(ntri);
+		int cnt = 0;
+		for (int f=0; f<nfacets(); f++) {
+		    if (3!=facet_size(f)) continue;
+		    for (int v=0; v<3; v++)
+		        tri.vert(cnt, v) = vert(f, v);
+		    ++cnt;
+		}
+	}
+
+	void Polygons::extract_quads(Quads &quads) {
+		quads = Quads();
+		quads.points = points;
+		int nquads = 0;
+		for (int f=0; f<nfacets(); f++)
+		    nquads += (4==facet_size(f));
+		quads.create_facets(nquads);
+		int cnt = 0;
+		for (int f=0; f<nfacets(); f++) {
+		    if (4!=facet_size(f)) continue;
+		    for (int v=0; v<4; v++)
+		        quads.vert(cnt, v) = vert(f, v);
+		    ++cnt;
+		}
+	}
+
+	int Polygons::nfacets() const {
+		return static_cast<int>(offset.size())-1;
+	}
+
+	int Polygons::facet_size(const int fi) const {
+		assert(fi>=0 && fi<nfacets());
+		return offset[fi+1]-offset[fi];
+	}
+
+	int Polygons::facet_corner(const int fi, const int ci) const {
+		assert(fi>=0 && fi<nfacets());
+		return offset[fi]+ci;
+	}
+
+	int Polygons::vert(const int fi, const int lv) const {
+		assert(fi>=0 && fi<nfacets());
+		int n = facet_size(fi);
+		assert(lv>=0 && lv<n);
+		return facets[offset[fi]+lv];
+	}
+
+	int &Polygons::vert(const int fi, const int lv) {
+		assert(fi>=0 && fi<nfacets());
+		int n = facet_size(fi);
+		assert(lv>=0 && lv<n);
+		return facets[offset[fi]+lv];
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	SurfaceConnectivity::SurfaceConnectivity(const Surface &p_m) : m(p_m) {
+		int nbc = m.ncorners();
+		c2f.resize(nbc, -1);
+		c2c.resize(nbc, -1);
+		v2c.resize(m.nverts(), -1);
+
+		for (int f=0; f<m.nfacets(); f++)
+		    for (int fc=0; fc<m.facet_size(f); fc++) {
+		        int c = m.facet_corner(f, fc);
+		        int v = m.vert(f, fc);
+		        c2f[c] = f;
+		        v2c[v] = c;
+		    }
+		for (int f=0; f<m.nfacets(); f++) // if it ain't broke, don't fix it
+		    for (int fc=0; fc<m.facet_size(f); fc++) {
+		        int c = m.facet_corner(f, fc);
+		        int v = m.vert(f, fc);
+		        c2c[c] = v2c[v];
+		        v2c[v] = c;
+		    }
+	}
+
+	int SurfaceConnectivity::from(const int corner_id) const {
+		int fi = c2f[corner_id];
+		int lv = corner_id - m.facet_corner(fi, 0);
+		return m.vert(fi, lv);
+	}
+
+	int SurfaceConnectivity::to(const int corner_id) const {
+		int fi = c2f[corner_id];
+		int lv = corner_id - m.facet_corner(fi, 0);
+		int n = m.facet_size(fi);
+		return m.vert(fi, (lv+1)%n);
+	}
+
+	int SurfaceConnectivity::next(const int corner_id) const {
+		int fi = c2f[corner_id];
+		int lv = corner_id - m.facet_corner(fi, 0);
+		int n = m.facet_size(fi);
+		return m.facet_corner(fi, (lv+1)%n);
+	}
+
+	int SurfaceConnectivity::prev(const int corner_id) const {
+		int fi = c2f[corner_id];
+		int lv = corner_id - m.facet_corner(fi, 0);
+		int n = m.facet_size(fi);
+		return m.facet_corner(fi, (lv-1+n)%n);
+	}
+
+	int SurfaceConnectivity::opposite(const int corner_id) const {
+		int cir = corner_id;
+		int result = -1; // not found
+		do {
+		    int candidate = prev(cir);
+		    if (from(candidate) == to(corner_id) && to(candidate) == from(corner_id)) {
+		        if (result == -1) result = candidate;
+		        else return -1; // found more than one
+		    }
+		    if (cir != corner_id && to(corner_id) == to(cir))
+		        return -1; // the edge is non manifold
+		    cir = c2c[cir];
+		} while (cir != corner_id);
+		return result;
+	}
+
+	bool SurfaceConnectivity::is_border_vert(const int v) const {
+		int cir = v2c[v];
+		if (cir<0) return false;
+		do {
+		    if (opposite(cir) == -1) return true;
+		    cir = c2c[cir];
+		} while (cir != v2c[v]);
+		return false;
+	}
+
+	int SurfaceConnectivity::next_around_vertex(const int corner_id) const {
+		return opposite(prev(corner_id));
+	}
+    */
+}
+
---
+++ ultimaille/volume.h
@@ -0,0 +1,110 @@
+#ifndef __VOLUME_H__
+#define __VOLUME_H__
+#include <vector>
+#include <memory>
+#include "geometry.h"
+#include "pointset.h"
+
+namespace UM {
+    struct GenericAttributeContainer;
+
+    struct Volume { // polyhedral mesh interface
+        PointSet points{};
+        std::vector<int> cells{};
+        ////	std::vector<std::weak_ptr<GenericAttributeContainer> > attr_facets{};
+        ////	std::vector<std::weak_ptr<GenericAttributeContainer> > attr_corners{};
+
+        Volume() = default;
+
+        ////	void delete_vertices(const std::vector<bool> &to_kill);
+        ////	virtual void delete_facets(const std::vector<bool> &to_kill);
+        void resize_attrs();
+        void compress_attrs(const std::vector<bool> &cells_to_kill);
+
+        int nverts() const;
+        virtual int ncells() const = 0;
+        //		int ncorners() const;
+
+        ////	virtual int nfacets() const = 0;
+        ////	virtual int facet_size(const int fi) const = 0;
+        ////	virtual int facet_corner(const int fi, const int ci) const = 0;
+        ////	virtual int  vert(const int fi, const int lv) const = 0;
+        ////	virtual int &vert(const int fi, const int lv)       = 0;
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    struct Tetrahedra : Volume { // simplicial mesh implementation
+        int ncells() const;
+        int create_tets(const int n);
+    };
+
+    struct Hexahedra : Volume { // hex mesh implementation
+        int ncells() const;
+        int create_hexa(const int n);
+    };
+/*
+
+	struct Triangles : Surface { // simplicial mesh implementation
+		int create_facets(const int n);
+
+		int nfacets()  const;
+		int facet_size(const int) const;
+		int facet_corner(const int fi, const int ci) const;
+		int  vert(const int fi, const int lv) const;
+		int &vert(const int fi, const int lv);
+	};
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	struct Quads : Surface { // quad mesh implementation
+		int create_facets(const int n);
+
+		int nfacets()  const;
+		int facet_size(const int) const;
+		int facet_corner(const int fi, const int ci) const;
+		int  vert(const int fi, const int lv) const;
+		int &vert(const int fi, const int lv);
+	};
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	struct Polygons : Surface { // polygonal mesh implementation
+		std::vector<int> offset;
+		Polygons();
+
+		int create_facets(const int n, const int size);
+		virtual void delete_facets(const std::vector<bool> &to_kill);
+		void extract_triangles(Triangles &tri);
+		void extract_quads(Quads &quads);
+
+		int nfacets()  const;
+		int facet_size(const int fi) const;
+		int facet_corner(const int fi, const int ci) const;
+		int  vert(const int fi, const int lv) const;
+		int &vert(const int fi, const int lv);
+	};
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	struct SurfaceConnectivity { // half-edge-like connectivity interface
+		SurfaceConnectivity(const Surface &p_m);
+
+		int from(const int corner_id) const;
+		int   to(const int corner_id) const;
+		int prev(const int corner_id) const;
+		int next(const int corner_id) const;
+		int opposite(const int corner_id) const;
+		bool is_border_vert(const int v) const;
+		int next_around_vertex(const int corner_id) const;
+
+		const Surface &m;
+		std::vector<int> v2c; // vertex to corner
+		std::vector<int> c2f; // corner to facet
+		std::vector<int> c2c; // corner to next corner sharing the same vertex (unordered)
+	};
+    */
+}
+
+#endif //__VOLUME_H__
+
