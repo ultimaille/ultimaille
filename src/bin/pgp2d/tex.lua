3e69c469eb431d17d25aec5eed552750f8b478a1
VM Francois <francois.protais@inria.fr>
2020-10-27 14:48:17 +0100

    restructuration of the project

--- src/bin/pgp2d/tex.lua
+++
@@ -1,3 +0,0 @@
-scene_graph.current().shader.painting='TEXTURE'
-scene_graph.current().shader.tex_image='/home/ssloy/+/ultimaille/src/bin/pgp2d/square-texture.png'
-scene_graph.current().shader.tex_coords='facet_corners.tex_coord'
