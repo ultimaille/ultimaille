8fdf5ff3f3d2f372c5359f12f6ed83b95201455c
Francois Protais <fprotais@ad.inria.fr>
2020-10-28 17:29:09 +0100

    More tweaks

--- test/pgp2d/pgp2d-corners.cpp
+++ test/pgp2d/pgp2d-corners.cpp
@@ -9,7 +9,7 @@
 #include <ultimaille/range.h>
 
 #include <OpenNL_psm/OpenNL_psm.h>
-
+# define M_PI          3.141592653589793238462643383279502884L
 using namespace UM;
 
 double average_edge_length(const Surface &m) {
