eb43cfb332263e11f811acdb96f5eee8b5161b89
Dmitry V. Sokolov <ha@haqr.eu>
2020-11-02 23:15:58 +0100

    range()/enumerate()/zip()

---
+++ test/test/test-range.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#include <ultimaille/all.h>
+
+using namespace UM;
+
+int main(int argc, char** argv) {
+    std::cerr << "testing for (int i : range(7)): " << std::endl;
+    for (int i : range(7))
+        std::cerr << i  << " ";
+    std::cerr << std::endl << std::endl;
+
+    std::cerr << "testing for (auto [i,v] : enumerate(array)): " << std::endl;
+    std::vector<std::string> array{"a", "b", "c", "d", "e", "f", "g"};
+    for (auto [i,v] : enumerate(array))
+        std::cerr << "(" << i << ",'" << v << "') ";
+    std::cerr << std::endl << std::endl;
+
+    std::cerr << "testing for (auto [u,v] : zip(array_a, array_b)): " << std::endl;
+    std::vector<int> array2{3, 1, 4, 1, 5, 9, 2};
+    for (auto [u,v] : zip(array2, array))
+        std::cerr << "(" << u << ",'" << v << "') ";
+    std::cerr << std::endl;
+
+    return 0;
+}
+
--- ultimaille/range.h
+++ ultimaille/range.h
@@ -6,90 +6,58 @@
 #include <iterator>
 
 namespace UM {
-	constexpr auto range(int n) {
-		struct iterator {
-		    int i;
-		    void operator++() { ++i; }
-		    bool operator!=(const iterator& rhs) const { return i != rhs.i; }
-		    const int &operator*() const { return i; }
-		};
-		struct wrapper {
-		    int n;
-		    auto begin() { return iterator{0}; }
-		    auto end()   { return iterator{n}; }
-		};
-		return wrapper{n};
-	}
+    constexpr auto range(int n) {
+        struct iterator {
+            int i;
+            void operator++() { ++i; }
+            bool operator!=(const iterator& rhs) const { return i != rhs.i; }
+            const int &operator*() const { return i; }
+        };
+        struct wrapper {
+            int n;
+            auto begin() { return iterator{0}; }
+            auto end()   { return iterator{n}; }
+        };
+        return wrapper{n};
+    }
 
-	template <typename T> constexpr auto enumerate(T && iterable) {
-		struct iterator {
-		    int i;
-		    T iterable;
-	//      typedef decltype(std::begin(std::declval<T>())) iterator_type;
-	//      iterator_type iter;
-		    bool operator!=(const iterator& rhs) const { return i != rhs.i; }
-		    void operator++() { ++i; /*++iter;*/ }
-		    auto operator*() const { return std::tie(i, *(iterable.begin()+i)); }
-		    auto begin() { return iterator{0, std::forward<T>(iterable)}; }
-		    auto end()   { return iterator{std::end(iterable)-std::begin(iterable), std::forward<T>(iterable)}; }
-		};
-		return iterator{0, std::forward<T>(iterable)};
-	}
+    // N.B. this implementation works only over random access containers (i.e. std::vector and not std::list)
+    template <typename T>
+        constexpr auto enumerate(T && iterable) {
+            struct iterator {
+                int i;
+                T iterable;
+                bool operator!=(const iterator& rhs) const { return i != rhs.i; }
+                void operator++() { ++i; }
+                auto operator*() const { return std::tie(i, *(iterable.begin()+i)); }
+                auto begin() { return iterator{0, std::forward<T>(iterable)}; }
+                auto end()   { return iterator{static_cast<int>(std::end(iterable)-std::begin(iterable)), std::forward<T>(iterable)}; }
+            };
+            return iterator{0, std::forward<T>(iterable)};
+        }
 
-	#if 0
-	template <typename... T> struct zip_helper {
-		struct iterator /* : std::iterator<std::forward_iterator_tag, std::tuple<decltype(*std::declval<T>().begin())...>>*/ {
-		      using value_ref_tuple_t = std::tuple<typename std::iterator_traits<T>::reference...>;
+    // N.B. if you need to zip more than two arrays, probably you are doing something wrong
+    template <typename T, typename U>
+        constexpr auto zip(T && t, U && u) {
+            struct iterator {
+                typedef decltype(std::begin(std::declval<T>())) iterator_type_T;
+                typedef decltype(std::begin(std::declval<U>())) iterator_type_U;
+                iterator_type_T itert;
+                iterator_type_U iteru;
 
-		        std::tuple<decltype(std::declval<T>().begin())...> iters_;
+                bool operator!=(const iterator& rhs) const { return itert != rhs.itert || iteru != rhs.iteru; }
+                void operator++() { ++itert; ++iteru; }
+                auto operator*() const { return std::make_pair(*itert, *iteru); }
+            };
+            struct wrapper {
+                T iterablet;
+                U iterableu;
+                auto begin() { return iterator{std::begin(iterablet), std::begin(iterableu)}; }
+                auto end()   { return iterator{std::end  (iterablet), std::end  (iterableu)}; }
+            };
 
-		        template <std::size_t... I>
-		            auto deref(std::index_sequence<I...>) const {
-		                return 0;
-	//typename iterator::value_type{*std::get<I>(iters_)...};
-		            }
-
-		        template <std::size_t... I>
-		            void increment(std::index_sequence<I...>) {
-		                auto l = {(++std::get<I>(iters_), 0)...};
-		            }
-
-		        explicit iterator(decltype(iters_) iters) : iters_{std::move(iters)} {}
-
-		        iterator& operator++() {
-		            increment(std::index_sequence_for<T...>{});
-		            return *this;
-		        }
-
-		        iterator operator++(int) {
-		            auto saved{*this};
-		            increment(std::index_sequence_for<T...>{});
-		            return saved;
-		        }
-
-		        bool operator!=(const iterator& other) const {
-		            return iters_ != other.iters_;
-		        }
-
-		        auto operator*() const { return deref(std::index_sequence_for<T...>{}); }
-		};
-
-		zip_helper(T&... seqs)
-		    : begin_{std::make_tuple(seqs.begin()...)},
-		    end_{std::make_tuple(seqs.end()...)} {}
-
-		iterator begin() const { return begin_; }
-		iterator end() const { return end_; }
-
-		iterator begin_;
-		iterator end_;
-	};
-
-	template <typename... T> constexpr auto zip(T && ... seqs) {
-		return zip_helper<T...>{seqs...};
-	}
-
-	#endif
+            return wrapper{std::forward<T>(t), std::forward<U>(u)};
+        }
 }
 #endif // __RANGE_H__
 
