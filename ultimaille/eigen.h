7fa90bbd2862df75a3e8239fdf6e2493e2a53290
Dmitry V. Sokolov <ha@haqr.eu>
2020-12-26 15:48:20 +0100

    comments

--- ultimaille/eigen.h
+++ ultimaille/eigen.h
@@ -6,12 +6,14 @@
 #include "quaternion.h"
 
 namespace UM {
-    // Analytic 2x2 eigensolver
     // Input:  symmetric matrix A
     // Output: eval - vector of eigenvalues, sorted by absolute value (decreasing), i.e. eval[0] is the largest magnitude eigenvalue
     //         evec - matrix of corresponding eigenvectors (columns) such that A = evec x diag(eval) evec^T
     //                The eigenvectors are guaranteed to be unit length and form a right-hand basis.
+
+    // Analytic 2x2 eigensolver
     void eigendecompose_symmetric(const mat2x2 &A, vec2 &eval, mat2x2 &evec);
+    // Iterative 3x3 eigensolver
     void eigendecompose_symmetric(const mat3x3 &A, vec3 &eval, mat3x3 &evec);
 }
 
