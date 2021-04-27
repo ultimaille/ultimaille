16aacc985b00d9defc97bfcb47dc297b35622a4b
Dmitry V. Sokolov <ha@haqr.eu>
2021-04-27 16:09:23 +0200

    openmp2 compatibility issue

--- ultimaille/syntactic-sugar/parallel.h
+++ ultimaille/syntactic-sugar/parallel.h
@@ -5,9 +5,11 @@
 
 namespace UM {
 
+#if defined(_OPENMP) && _OPENMP>=200805
 #pragma omp declare reduction(vec_double_plus : std::vector<double> : \
         std::transform(omp_out.begin(), omp_out.end(), omp_in.begin(), omp_out.begin(), std::plus<double>())) \
     initializer(omp_priv = std::vector<double>(omp_orig.size(), 0))
+#endif
 
     struct SpinLock {
         SpinLock() : flag{ATOMIC_FLAG_INIT} {}
