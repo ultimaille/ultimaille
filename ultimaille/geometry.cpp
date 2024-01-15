a4d95b36dca7530e090d524aaf01d8c5c812d193
lbinria <77280433+lbinria@users.noreply.github.com>
2024-01-15 12:04:48 +0100

    Clean up

--- ultimaille/geometry.cpp
+++ ultimaille/geometry.cpp
@@ -40,16 +40,20 @@ namespace UM {
         return UM::bary_verts(v.data(), static_cast<const int>(v.size()));
     }
 
+    vec3 Poly3::normal() const {
+        return UM::normal(v.data(), static_cast<const int>(v.size()));
+    }
+
+    double Poly3::unsigned_area() const {
+        return UM::unsigned_area(v.data(), static_cast<const int>(v.size()));
+    }
+
     vec3 Tetrahedron3::bary_verts() const {
         return UM::bary_verts(v, 4);
     }
 
     vec3 Hexahedron3::bary_verts() const {
         return UM::bary_verts(v, 8);
-    }
-
-    vec3 Pyramid3::bary_verts() const {
-        return UM::bary_verts(v, 5);
     }
 
     double Hexahedron3::volume() const {
@@ -82,6 +86,10 @@ namespace UM {
         return vol;
     }
 
+    vec3 Pyramid3::bary_verts() const {
+        return UM::bary_verts(v, 5);
+    }
+
     double Pyramid3::volume() const {
 
         // Indexes of pyramid base
@@ -103,14 +111,6 @@ namespace UM {
         }
 
         return vol;
-    }
-
-    vec3 Poly3::normal() const {
-        return UM::normal(v.data(), static_cast<const int>(v.size()));
-    }
-
-    double Poly3::unsigned_area() const {
-        return UM::unsigned_area(v.data(), static_cast<const int>(v.size()));
     }
 
 }
