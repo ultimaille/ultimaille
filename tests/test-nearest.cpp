6c379a711bd3f370c9b2681e500b7ced1990c9a2
Dmitry V. Sokolov <ha@haqr.eu>
2024-09-23 15:25:33 +0200

    catch, really?

--- tests/test-nearest.cpp
+++ tests/test-nearest.cpp
@@ -11,7 +11,7 @@ TEST_CASE("projection to a 3d triangulated surface", "[nearest]") {
     NearestPointOnMesh npm(m);
 
     PointOnMesh p1 = npm.query({0.8311, 0.0224465, 1.00753});
-    CHECK( (p1.f == 2) || (p1.f == 3) ); // nearest point is on an edge between facets 2 and 3
+    CHECK( ((p1.f == 2) || (p1.f == 3)) ); // nearest point is on an edge between facets 2 and 3
     CHECK( (vec3{0.411785, 0, 0.588215} - static_cast<vec3>(p1)).norm() < 1e-5 );
 
     auto p2 = npm.query({0.802483, 1.14223, 1.26074});
@@ -19,7 +19,7 @@ TEST_CASE("projection to a 3d triangulated surface", "[nearest]") {
     CHECK( (vec3{0.067334, 0.407079, 0.525587} - static_cast<vec3>(p2)).norm() < 1e-5 );
 
     auto p3 = npm.query({1.2124, -0.118379, 0.194748});
-    CHECK( (p3.f == 1) || (p3.f == 2) || (p3.f == 3) ); // nearest is on the vertex between facets 1, 2 and 3
+    CHECK( ((p3.f == 1) || (p3.f == 2) || (p3.f == 3)) ); // nearest is on the vertex between facets 1, 2 and 3
     CHECK( (vec3{1, 0, 0} - static_cast<vec3>(p1)).norm() < 1e-5 );
 }
 
