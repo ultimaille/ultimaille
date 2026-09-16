a1a09bc4229886a4e55ab6924a570458e9c3bf9f
Dmitry Sokolov <ha@haqr.eu>
2026-09-16 15:32:42 +0200

    better test

--- tests/test-multimesh.cpp
+++ tests/test-multimesh.cpp
@@ -5,14 +5,16 @@ using namespace UM;
 
 TEST_CASE("Shared pointset editing", "[MultiMesh]") {
     Triangles m1;
-    *m1.points.data = {{1, 0, 0}, { 0.309017, 0.951057, 0}, {-0.809017, 0.587785, 0}};
-    m1.facets = {0,1,2};
+    *m1.points.data = {{-1,-1,-1}, {1, 0, 0}, { 0.309017, 0.951057, 0}, {-0.809017, 0.587785, 0}};
+    m1.facets = {1,2,3};
+
+    write_by_extension("m1-.geogram", m1);
 
     PointAttribute<int> a1(m1.points);
-    for (int i=0; i<3; i++)
+    for (int i=0; i<4; i++)
         a1[i] = i;
 
-    REQUIRE( a1.ptr->data.size() == 3 );
+    REQUIRE( a1.ptr->data.size() == 4 );
 
     Quads m2;
     m2.points = m1.points;
@@ -20,16 +22,23 @@ TEST_CASE("Shared pointset editing", "[MultiMesh]") {
     PointAttribute<int> a2(m2.points);
     m2.points.push_back({-0.809017,-0.587785,0});
     m2.points.push_back({ 0.309017,-0.951057,0});
-    m2.facets = {0,2,3,4};
+    m2.facets = {1,3,4,5};
 
-    REQUIRE( a1.ptr->data.size() == 5 );
-    REQUIRE( a2.ptr->data.size() == 5 );
+    REQUIRE( a1.ptr->data.size() == 6 );
+    REQUIRE( a2.ptr->data.size() == 6 );
 
-    for (int i=3; i<5; i++)
+    for (int i=4; i<6; i++)
         a2[i] = i;
 
-    m1.points.push_back({-1,-1,-1});
-    m2.points.delete_points([&](int id) { return id==5; });
+    std::vector<int> old2new;
+    m2.points.delete_points([&](int id) { return id==0; }, old2new);
+
+    for (auto v : range(3))
+        m1.vert(0, v) = old2new[m1.vert(0, v)];
+
+    for (auto v : range(4))
+        m2.vert(0, v) = old2new[m2.vert(0, v)];
+
     REQUIRE( m1.points.size() == 5 );
 
     write_by_extension("m1.geogram", m1, {{"a1", a1}, {"a2", a2}});
